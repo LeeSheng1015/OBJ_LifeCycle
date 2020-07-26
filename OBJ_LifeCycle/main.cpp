45b4019fc816a59e52f1d9f09399232966b92af6
LeeSheng <727151354@qq.com>
2020-07-26 16:46:33 +0800

    Initial Commit

---
+++ OBJ_LifeCycle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OBJ_LifeCycle
+//
+//  Created by Lee Sheng on 2020/7/26.
+//  Copyright © 2020 Lee Sheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
